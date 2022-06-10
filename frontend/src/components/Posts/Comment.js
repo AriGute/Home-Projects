55ba5d0ded58edb3c24848e299cfc689563993f9
orshokety <orshokety@gmail.com>
2022-06-10 16:16:36 +0300

    Done

--- frontend/src/components/Posts/Comment.js
+++ frontend/src/components/Posts/Comment.js
@@ -2,38 +2,73 @@ import CardProfile from '../CardProfile/CardProfile';
 import Utils from '../../services/Utils';
 import './Comment.css';
 import ToolTip from '../ToolTip';
+import { useState } from 'react';
+import PostService from '../../services/PostService';
 
 const Comment = ({ comment }) => {
-	const date = Utils.DateFormat(comment.creationDate);
+
+	const [commentText, setCommentText] = useState(comment.comment);
+	const [isEditing, setIsEditing] = useState(false);
+	const [date , setDate] = useState(Utils.DateFormat(comment.lastModifiedDate))
+
 	const editComment = () => {
-		alert('edit');
+		setIsEditing(true);
 	};
+
 	const deleteComment = () => {
 		alert('delete');
 	};
+
 	const reportComment = () => {
 		alert('report');
 	};
+
+	const editedComment = (e) => {
+		e.preventDefault()
+		setIsEditing(false)
+	    PostService.UpdateComment(comment._id, commentText)
+		setDate(Utils.DateFormat(Date()))
+	};
+
 	return (
 		<div className='card'>
 			<div className='commentContent'>
 				<CardProfile profile={comment.ownerProfile}></CardProfile>
-				<div className='commentText'>
-					{comment.comment.split('\n').map((text, i) => {
-						//split every line in text to p
-						return (
-							<p
-								style={{
-									marginBottom: '0px',
-									marginTop: '0px',
-								}}
-								key={comment._id + i}>
-								{text}
-							</p>
-						);
-					})}
-					<p className='date'>{date}</p>
-				</div>
+				{isEditing ? (
+						<form onSubmit={editedComment} className='editContainer'>
+						<textarea
+							className='editing'
+							name=''
+							id=''
+							cols='30'
+							value={commentText}
+							onChange={(e) => {
+								setCommentText(e.target.value)
+							}}>
+						</textarea>
+						<button className='edditBtn' type='submit'>
+							Save
+						</button>
+						</form>
+				) : (
+					<div className='commentText'>
+						{commentText.split('\n').map((text, i) => {
+							//split every line in text to p
+							return (
+								<p
+									style={{
+										marginBottom: '0px',
+										marginTop: '0px',
+									}}
+									key={comment._id + i}>
+									{text}
+								</p>
+							);
+						})}
+
+						<p className='date'>{date}</p>
+					</div>
+				)}
 				<ToolTip edit={editComment} del={deleteComment}>
 					report={reportComment}
 				</ToolTip>
