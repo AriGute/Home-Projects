d9f9d7580eda7c164cff1297a09379a4b0d06f95
Ariel Gueta <arielguata@gmail.com>
2022-07-29 00:10:25 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use('/auth', cors({ credentials: true }), authService.routes);
 
 app.use('/posts', cors({ credentials: true }), postService.routes);
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
